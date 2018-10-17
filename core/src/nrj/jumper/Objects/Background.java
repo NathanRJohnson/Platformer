6f64008ad30ff074c68ab92bf63890a72342af84
Nathan Johnson <robert.minecrafter.nathan@gmail.com>
2018-10-16 21:13:16 -0400

    Initial-er Commit

---
+++ core/src/nrj/jumper/Objects/Background.java
@@ -0,0 +1,15 @@
+package nrj.jumper.Objects;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+public class Background extends Sprite {
+
+    public Background(Texture tx) {
+        super(tx);
+        int nWidth = 700, nHeight = 500;
+        setSize(nWidth, nHeight);
+        setPosition(0,0);
+
+    }
+}
